cabc8b714d67bee55e325e54a02c59f68601d025
Eric Donald <ericdon@bu.edu>
2026-03-06 17:13:18 -0500

    openpyxl package

--- Code/Executor.py
+++ Code/Executor.py
@@ -70,7 +70,7 @@ P.Σ_robust(0.95, 0.1)
 # ----------------------- #
 # Record Package Versions #
 # ----------------------- #
-packages = ["cyipopt", "ipumspy", "numba", "numpy", "pandas", "quantecon", "scipy"]
+packages = ["cyipopt", "ipumspy", "numba", "numpy", "openpyxl", "pandas", "quantecon", "scipy"]
 P.write_package_versions(packages)
 
 
